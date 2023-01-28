f3adce43c2efa167784c2c04345a67ee18b98e69
甚吊圣人 <1321075268@qq.com>
2023-01-28 22:19:17 +0800

    [更新]BGD梯度下降--模型对比

--- 第四周考核/main.py
+++ 第四周考核/main.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LinearRegression
 from sklearn import datasets
 import numpy as np
 
@@ -17,11 +18,26 @@ def grad_des(theta, X, Y, alpha):
     return theta
 
 diabetes = datasets.load_diabetes()
-m = int(len(diabetes.data) * 0.7)
-data = diabetes.data[:m]
-data = np.concatenate((np.ones((len(data), 1)), data), axis=1)
-target = diabetes.target[:m]
-Alpha = 0.01
-Theta = np.ones((len(data[0]), 1))
-Theta = grad_des(Theta, data, target.reshape(m, 1), Alpha)
-print(Theta)
+data = diabetes.data
+m = len(data)
+data_ = np.concatenate((np.ones((len(data), 1)), data), axis=1)
+target = diabetes.target
+Alpha = 1.9
+Theta = np.ones((len(data_[0]), 1))
+Theta = grad_des(Theta, data_, target.reshape(m, 1), Alpha)
+print("自己的系数：", Theta.T[0][1:])
+print("自己的截距：", Theta[0][0])
+print('-'*10)
+lr = LinearRegression()
+lr.fit(data, target)
+print("sklearn拟合系数", lr.coef_)
+print("sklearn截距", lr.intercept_)
+'''
+>>>自己的系数： [  -9.62569502 -239.38549127  520.81674785  324.00889739 -708.11349663
+                410.03694915   63.44426249  166.33351198  719.95395432   67.93464   ]
+>>>自己的截距： 152.13348416289648
+----------
+>>>sklearn拟合系数 [ -10.01219782 -239.81908937  519.83978679  324.39042769 -792.18416163
+                    476.74583782  101.04457032  177.06417623  751.27932109   67.62538639]
+>>>sklearn截距 152.1334841628965
+'''

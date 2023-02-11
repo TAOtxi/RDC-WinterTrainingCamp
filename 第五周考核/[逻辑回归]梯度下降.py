a07aae22a7156747c3872de158e6bf3085d05957
甚吊圣人 <1321075268@qq.com>
2023-02-11 12:41:02 +0800

    [逻辑回归]修改正则化项系数

--- 第五周考核/[逻辑回归]梯度下降.py
+++ 第五周考核/[逻辑回归]梯度下降.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 
@@ -27,10 +28,11 @@ class LogiReg:
         new_loss, Hx = self.loss_func(X, Y, theta)
         while True:
             old_loss = new_loss
-            gradient = np.dot(X.T, (Hx - Y)) + self.C * theta
-            theta = theta - alpha * gradient
+            gradient = np.dot(X.T, Hx - Y)
+            theta[1:] = theta[1:] - alpha * gradient[1:] - self.C * theta[1:]
+            theta[0] = theta[0] - alpha * gradient[0]
             new_loss, Hx = self.loss_func(X, Y, theta)
-            if np.abs(old_loss - new_loss) < self.tol:
+            if np.abs(old_loss - new_loss) <= self.tol:
                 break
         self.coef_, self.intercept_ = theta.T[0][1:], theta[0][0]
 
@@ -38,28 +40,28 @@ class LogiReg:
 data, target = load_breast_cancer(return_X_y=True)
 MinMax = MinMaxScaler()
 data = MinMax.fit_transform(data)
-Alpha = 0.08
-Logi = LogiReg(C=1000)
-Logi.fit(data, target, Alpha)
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+Alpha = 0.01
+Logi = LogiReg(C=100)
+Logi.fit(X_train, y_train, Alpha)
 print("自己的系数：", Logi.coef_)
 print("自己的截距：", Logi.intercept_)
-lg = LogisticRegression(C=1000, max_iter=10000)
-lg.fit(data, target)
+lg = LogisticRegression(C=1, max_iter=10000)
+lg.fit(X_train, y_train)
 print("sklearn系数：", lg.coef_)
 print("sklearn的截距：", lg.intercept_)
 '''
-自己的系数： [  5.92558021   0.54299262   5.86943542  -0.69255307  -2.01973081
-  15.80942231  -8.58706681 -14.55350294   3.48866207   0.22401657
- -26.73851114   5.89512604 -12.82059101 -17.58909549  -5.3846213
-   6.12742599   6.21098646  -4.5698809    3.57774081  11.64347318
- -17.85642678 -16.59878288 -11.02334938 -18.7494114   -4.18976916
-   1.78280396  -7.85527731  -7.45383861 -12.30683538  -8.68464202]
-自己的截距： 27.121661540979773
-sklearn系数： [[ 10.64713386   0.53368336   9.34884602   2.57646577  -4.79190463
-   22.51835097 -13.36678792 -15.50968242   2.47997528  -2.13443468
-  -27.68460401   6.94440344  -1.28740596 -23.37537453  -6.20132794
-   -1.45577365  17.37335643 -14.62667779   4.55735868  28.66638553
-  -24.27665668 -17.31834974 -14.03364935 -28.21649484  -1.83884119
-    3.54700919  -9.00028957  -3.19415196 -11.61879133 -16.12910528]]
-sklearn的截距： [28.23862808]
+自己的系数： [-1.68996565 -1.54461771 -1.65833756 -1.41862139 -0.6785336  -0.28703816
+ -1.20781793 -1.86815073 -0.44774915  0.79217682 -1.15347494 -0.17694112
+ -0.93070171 -0.74083066  0.05984187  0.49851498  0.21579325 -0.27566912
+  0.32938211  0.42502471 -2.12586484 -2.26676343 -1.97683912 -1.53871128
+ -1.46535716 -0.7718679  -1.24470468 -2.43519816 -1.10214263 -0.42421869]
+自己的截距： 8.304687331323855
+sklearn系数： [[-1.69250262 -1.54575053 -1.66055816 -1.41955616 -0.68047076 -0.28617921
+  -1.20644206 -1.86636789 -0.44904646  0.79066093 -1.15282176 -0.17733811
+  -0.93018832 -0.74029397  0.058992    0.49832546  0.21504262 -0.27683136
+   0.32919078  0.42459568 -2.1269972  -2.26769602 -1.97773967 -1.53867832
+  -1.46643419 -0.77149793 -1.24497291 -2.43512743 -1.10265907 -0.42463936]]
+sklearn的截距： [8.30976597]
+
 '''
